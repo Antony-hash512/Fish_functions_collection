742bb30b7a652b3e8816f8bedc89d6f0decaf157
Antony <antony.ictr@gmail.com>
2026-05-10 01:00:24 +0400

    update

--- functions/upscale_video_realesrgan.fish
+++ functions/upscale_video_realesrgan.fish
@@ -1,10 +1,34 @@
-function upscale_video_realesrgan --description 'Upscale video to 1080x1920 using realesrgan-ncnn-vulkan'
-    set input_video $argv[1]
-    set output_video $argv[2]
+function upscale_video_realesrgan --description 'Upscale video using realesrgan-ncnn-vulkan'
+    argparse 'r/res=' 's/style=' 'h/help' -- $argv
+    or return 1
 
-    if test -z "$input_video"; or test -z "$output_video"
-        echo "Использование: upscale_video_realesrgan <входящее_видео.mp4> <исходящее_видео.mp4>"
+    if set -ql _flag_help
+        echo "Использование: upscale_video_realesrgan [опции] <входящее_видео> <исходящее_видео>"
+        echo "Опции:"
+        echo "  -r, --res     Разрешение: 1080p, 2k, 4k (по умолчанию: 2k)"
+        echo "  -s, --style   Стиль: anime, photo (по умолчанию: anime)"
+        echo "  -h, --help    Показать эту справку"
+        return 0
+    end
+
+    if test (count $argv) -lt 2
+        echo "Ошибка: Не указаны входящее и исходящее видео."
+        echo "Использование: upscale_video_realesrgan [опции] <входящее_видео> <исходящее_видео>"
         return 1
+    end
+
+    set -l input_video $argv[1]
+    set -l output_video $argv[2]
+
+    # Значения по умолчанию
+    set -l resolution "2k"
+    if set -ql _flag_res
+        set resolution $_flag_res
+    end
+
+    set -l style "anime"
+    if set -ql _flag_style
+        set style $_flag_style
     end
 
     # Проверяем зависимости
@@ -13,25 +37,63 @@ function upscale_video_realesrgan --description 'Upscale video to 1080x1920 usin
         return 1
     end
 
+    # Конфигурация стиля
+    set -l model_name
+    switch $style
+        case anime
+            set model_name "realesrgan-x4plus-anime"
+        case photo
+            set model_name "realesrgan-x4plus"
+        case '*'
+            echo "Ошибка: Неизвестный стиль '$style'. Доступные стили: anime, photo."
+            return 1
+    end
+
+    # Конфигурация разрешения
+    set -l scale_factor
+    set -l tile_size
+    set -l thread_configuration
+    set -l ffmpeg_scale_filter
+
+    switch $resolution
+        case 1080p
+            set scale_factor 2
+            set tile_size 512
+            set thread_configuration "2:2:2"
+            set ffmpeg_scale_filter -vf "scale=1080:1920"
+        case 2k
+            set scale_factor 2
+            set tile_size 512
+            set thread_configuration "2:2:2"
+            # Для 2k без фильтра масштабирования
+        case 4k
+            set scale_factor 4
+            set tile_size 256
+            set thread_configuration "1:2:2"
+            set ffmpeg_scale_filter -vf "scale=2160:3840"
+        case '*'
+            echo "Ошибка: Неизвестное разрешение '$resolution'. Доступные разрешения: 1080p, 2k, 4k."
+            return 1
+    end
+
     # Создаем временную директорию
-    set tmp_dir (mktemp -d)
-    set frames_in "$tmp_dir/frames_in"
-    set frames_out "$tmp_dir/frames_out"
-    mkdir -p $frames_in $frames_out
+    set -l temporary_directory (mktemp -d)
+    echo "Временная директория: $temporary_directory"
+    set -l frames_input_directory "$temporary_directory/frames_in"
+    set -l frames_output_directory "$temporary_directory/frames_out"
+    mkdir -p $frames_input_directory $frames_output_directory
 
     echo "[1/4] Извлекаем кадры из $input_video..."
-    ffmpeg -loglevel error -stats -i $input_video -qscale:v 1 -qmin 1 -qmax 1 "$frames_in/frame_%08d.jpg"
+    ffmpeg -loglevel error -stats -i $input_video "$frames_input_directory/frame_%08d.png"
 
-    echo "[2/4] Апскейлим кадры через Vulkan (x4)..."
-    # -n: используем универсальную модель x4plus
-    # -s: масштабируем в 4 раза
-    realesrgan-ncnn-vulkan -i $frames_in -o $frames_out -n realesrgan-x4plus -s 4 -f jpg
+    echo "[2/4] Апскейлим кадры через Vulkan..."
+    realesrgan-ncnn-vulkan -i $frames_input_directory -o $frames_output_directory -n $model_name -s $scale_factor -t $tile_size -j $thread_configuration -f png
 
-    echo "[3/4] Собираем видео 1080x1920 и возвращаем звук..."
-    ffmpeg -loglevel error -stats -framerate 24 -i "$frames_out/frame_%08d.jpg" -i $input_video -map 0:v:0 -map 1:a:0? -c:v libx264 -crf 18 -pix_fmt yuv420p -c:a copy -vf "scale=1080:1920" $output_video
+    echo "[3/4] Собираем видео и возвращаем звук..."
+    ffmpeg -loglevel error -stats -framerate 24 -i "$frames_output_directory/frame_%08d.png" -i $input_video -map 0:v:0 -map 1:a:0? -c:v libx264 -crf 18 -pix_fmt yuv420p -c:a copy $ffmpeg_scale_filter $output_video
 
     echo "[4/4] Очищаем временные файлы..."
-    rm -rf $tmp_dir
+    rm -rf $temporary_directory
 
-    echo "Успешно! Идеальный Shorts сохранен в $output_video"
+    echo "Успешно! Видео сохранено в $output_video"
 end
